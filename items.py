7d3ac1ac69946e1738a3ee8b7592ef8d82564ded
Santeri Silvennoinen <santeri.silvennoinen@helsinki.fi>
2025-03-27 17:15:10 +0200

    Add edit item feature

--- items.py
+++ items.py
@@ -10,11 +10,20 @@ def get_items():
     return db.query(sql)
 
 def get_item(item_id):
-    sql = """SELECT items.title,
+    sql = """SELECT items.id,
+                    items.title,
                     items.location,
                     items.description,
+                    users.id user_id,
                     users.username
              FROM items, users
              WHERE items.user_id = users.id AND
                    items.id = ?"""
     return db.query(sql, [item_id])[0]
+
+def update_item(item_id, title, location, description):
+    sql = """UPDATE items SET title = ?,
+                              location = ?,
+                              description = ?
+                          WHERE id = ?"""
+    db.execute(sql, [title, location, description, item_id])
